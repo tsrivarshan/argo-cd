14e3d8c32782aae62fb8680efffc16c6b5cc3435
krishnansai <krishnansai71@gmail.com>
2023-03-17 17:36:59 +0530

    cd

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl get all -n argocd
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl get services --namespace argocd argocd-server --output jsonpath='{.status.loadBalancer.ingress[0].ip}' | echo
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
